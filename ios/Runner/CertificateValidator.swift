7c8258870cda3b82f8b557071b0ea64cbdf2c0ff
Temerov Anton <hurdlelightsick@gmail.com>
2023-10-05 21:17:33 +0700

    Bim bim bam bam

---
+++ ios/Runner/CertificateValidator.swift
@@ -0,0 +1,71 @@
+import Foundation
+
+@available(iOS 13.0.0, *)
+actor CertificateValidator {
+    var certificates = [SecCertificate]()
+    
+    func prepareCertificates(_ names: [String]) {
+        certificates = names.compactMap(certificate(name:))
+    }
+    
+    private func certificate(name: String) -> SecCertificate? {
+        if let path = Bundle.main.url(forResource: name, withExtension: "der", subdirectory: "Certificates"),
+           let certData = try? Data(contentsOf: path),
+           let certificate = SecCertificateCreateWithData(nil, certData as CFData) {
+            return certificate
+        } else {
+            return nil
+        }
+
+    }
+    
+    func isCertificatesValid(at date: Date) -> Bool {
+        for certificate in certificates {
+            var trust: SecTrust!
+            _ = SecTrustCreateWithCertificates(certificate, nil, &trust)
+            
+            let _ = SecTrustSetVerifyDate(trust, date as CFDate)
+            
+            if !checkValidity(of: trust!) {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func checkValidity(of serverTrust: SecTrust, anchorCertificatesOnly: Bool = false) -> Bool {
+        SecTrustSetAnchorCertificates(serverTrust, certificates as CFArray)
+        SecTrustSetAnchorCertificatesOnly(serverTrust, anchorCertificatesOnly)
+        var error: CFError?
+        
+        if #available(iOS 12.0, *) {
+            let isTrusted = SecTrustEvaluateWithError(serverTrust, &error)
+            return isTrusted
+        } else {
+            print("Бим бим бам бам")
+            return false
+        }
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- ios/Runner/FLNativeView.swift
+++ ios/Runner/FLNativeView.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import WebKit
 
 class FLNativeViewFactory: NSObject, FlutterPlatformViewFactory {
     private var messenger: FlutterBinaryMessenger
@@ -14,11 +15,16 @@ class FLNativeViewFactory: NSObject, FlutterPlatformViewFactory {
         viewIdentifier viewId: Int64,
         arguments args: Any?
     ) -> FlutterPlatformView {
-        return FLNativeView(
-            frame: frame,
-            viewIdentifier: viewId,
-            arguments: args,
-            binaryMessenger: messenger)
+        if #available(iOS 13.0.0, *) {
+            return FLNativeView(
+                frame: frame,
+                viewIdentifier: viewId,
+                arguments: args,
+                binaryMessenger: messenger)
+        } else {
+            let mes = "zalupa"
+            return mes as! FlutterPlatformView
+        }
     }
 
     /// Implementing this method is only necessary when the `arguments` in `createWithFrame` is not `nil`.
@@ -27,8 +33,11 @@ class FLNativeViewFactory: NSObject, FlutterPlatformViewFactory {
     }
 }
 
-class FLNativeView: NSObject, FlutterPlatformView {
+@available(iOS 13.0.0, *)
+class FLNativeView: NSObject, FlutterPlatformView, WKNavigationDelegate {
     private var _view: UIView
+    private var webView: WKWebView
+    let validator = CertificateValidator()
 
     init(
         frame: CGRect,
@@ -36,8 +45,12 @@ class FLNativeView: NSObject, FlutterPlatformView {
         arguments args: Any?,
         binaryMessenger messenger: FlutterBinaryMessenger?
     ) {
+        let fullHeight = UIScreen.main.bounds.height
+        let fullWidth = UIScreen.main.bounds.width
         _view = UIView()
+        webView = WKWebView(frame: CGRect(x: 0, y: 0, width: fullWidth, height: fullHeight))
         super.init()
+
         // iOS views can be created here
         createNativeView(view: _view)
     }
@@ -47,12 +60,40 @@ class FLNativeView: NSObject, FlutterPlatformView {
     }
 
     func createNativeView(view _view: UIView){
-        _view.backgroundColor = UIColor.blue
-        let nativeLabel = UILabel()
-        nativeLabel.text = "Native text from iOS"
-        nativeLabel.textColor = UIColor.white
-        nativeLabel.textAlignment = .center
-        nativeLabel.frame = CGRect(x: 0, y: 0, width: 180, height: 48.0)
-        _view.addSubview(nativeLabel)
+        
+        webView.navigationDelegate = self
+
+        Task {
+                   let names = ["Russian Trusted Root CA"]
+                   await validator.prepareCertificates(names)}
+        let url = URL(string: "https://vk.com/")!
+                webView.load(URLRequest(url: url))
+        
+        _view.addSubview(webView)
     }
+    
+    func webView(
+            _ webView: WKWebView,
+            didReceive challenge: URLAuthenticationChallenge,
+            completionHandler: @escaping (URLSession.AuthChallengeDisposition,
+                                          URLCredential?) -> Void
+        ) {
+            guard let serverTrust = challenge.protectionSpace.serverTrust
+            else { return completionHandler(.performDefaultHandling, nil) }
+            
+            if #available(iOS 13.0, *) {
+                Task.detached(priority: .userInitiated) {
+                    if await self.validator.checkValidity(of: serverTrust) {
+                        // Allow our sertificate
+                        let cred = URLCredential(trust: serverTrust)
+                        completionHandler(.useCredential, cred)
+                    } else {
+                        // Default check for another connections
+                        completionHandler(.performDefaultHandling, nil)
+                    }
+                }
+            } else {
+                // Fallback on earlier versions
+            }
+        }
 }
